93a008af120d8a37eb36f835ab0b4ab0afc85ded
NAYOTO. <17558264+nayotodot@users.noreply.github.com>
2025-03-16 19:31:00 +0900

    Update `unpack`

--- unpack.lua
+++ unpack.lua
@@ -3,9 +3,9 @@ local table = require "table";
 local getn = table.getn;
 
 local function unpack(list, i, j)
-	local i = i or 1;
-	local j = j or getn(list);
-	if i <= j and list[i] then
+	i = i or 1;
+	j = j or getn(list);
+	if i <= j then
 		return list[i], unpack(list, i + 1, j);
 	end
 end
